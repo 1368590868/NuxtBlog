8e236b81ec89408003ec91f47cc9839796f39efc
1368590868 <1368590868@qq.com>
2020-04-04 10:25:09 +0800

    Carousel Change ImgList

--- components/aritcleCarousel.vue
+++ components/aritcleCarousel.vue
@@ -13,10 +13,11 @@
   export default {
    data(){
      return{
-       imgList: ['http://img1.imgtn.bdimg.com/it/u=3062374190,3164523231&fm=11&gp=0.jpg',
-                  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585163963088&di=7b162d70f8f0981bf8c3398a4a178392&imgtype=0&src=http%3A%2F%2Fdik.img.kttpdq.com%2Fpic%2F28%2F19439%2F4f03e27728caa5be.jpg',
-                  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585164042057&di=c41d1d286c0f68a8fba65c045e6ce31c&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201407%2F05%2F20140705171243_tuVKa.jpeg',
-                  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585164080010&di=b9b9c4d78e6efea2a08586b7c9b70e3e&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2Fb%2F54631d4959556.jpg'
+       imgList: ['http://cdn.irlin.cn/%E8%BD%AE%E6%92%AD%E5%9B%BE%E4%B8%80.jpg',
+                  'http://cdn.irlin.cn/%E8%BD%AE%E6%92%AD%E5%9B%BE%E4%B8%89.jpg',
+                  'http://cdn.irlin.cn/%E8%BD%AE%E6%92%AD%E5%9B%BE%E4%BA%8C.jpg',
+                  'http://cdn.irlin.cn/%E8%BD%AE%E6%92%AD%E5%9B%BE%E4%BA%94.jpg',
+                  'http://cdn.irlin.cn/%E8%BD%AE%E6%92%AD%E5%9B%BE%E5%9B%9B.jpg'
        ]
      }
    }
